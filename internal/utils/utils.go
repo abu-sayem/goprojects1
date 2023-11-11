1b897daaeb1cfe88018dea1664f04fb7dad467e0
abu-sayem <abusaayem@gmail.com>
2023-11-12 04:08:02 +0600

    Add RandomString function to generate random strings of length n

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -15,6 +15,7 @@ func RandomInt(min, max int) int {
 	return min + rand.Intn(max-min)
 }
 
+// RandomString generates a random string of length n
 func RandomString(n int) string {
 	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 	b := make([]rune, n)
